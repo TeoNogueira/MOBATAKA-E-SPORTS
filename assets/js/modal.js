cb4736582018839c699b2a4f1dea95d943699b69
Teo Nogueira <teonogueira17@gmail.com>
2023-06-30 20:32:15 -0300

    mobataka_change

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,10 +1,39 @@
+const mod01 = document.querySelector('.op-01');
+const mod02 = document.querySelector('.op-02');
+
+
+
+
+function setLinks(type) {
+
+const links = {
+
+  link01: ["uDG63MEOxmI"],
+  link02: ["4o5BMp3SY-g"],
+
+}
+
+mod01.setAttribute('src', `https://www.youtube.com/embed/${links[type][0]}`);
+mod02.setAttribute('src', `https://www.youtube.com/embed/${links[type][1]}`);
+
+
+
+
+}
+
+
+
+
+
 /*=============== SHOW MODAL ===============*/
 const showModal = (openButton, modalContent) =>{
     const openBtn = document.getElementById(openButton),
     modalContainer = document.getElementById(modalContent)
-    
+
     if(openBtn && modalContainer){
-        openBtn.addEventListener('click', ()=>{
+     
+        openBtn.addEventListener('click', (e)=>{
+            e.preventDefault();
             modalContainer.classList.add('show-modal')
         })
     }
