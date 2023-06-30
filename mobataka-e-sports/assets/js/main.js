bd2a4c70783310ca71b541905b697629e5a39757
Teo Nogueira <teonogueira17@gmail.com>
2023-06-30 17:22:29 -0300

    vite_conf_modal_&_website

--- mobataka-e-sports/assets/js/main.js
+++ mobataka-e-sports/assets/js/main.js
@@ -1,3 +1,10 @@
+// Imports
+
+import '../css/main.css';
+import './modal.js';
+
+
+
 /*=============== FILTERS TABS ===============*/
 const tabs = document.querySelectorAll('[data-target]'),
       tabContents = document.querySelectorAll('[data-content]')
@@ -49,18 +56,18 @@ themeButton.addEventListener('click', () => {
 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-const sr = ScrollReveal({
+const sc = ScrollReveal({
     origin: 'top',
     distance: '60px',
     duration: 2500,
     delay: 400,
 })
 
-sr.reveal(`.profile__border`)
-sr.reveal(`.profile__name`, {delay: 500})
-sr.reveal(`.profile__profession`, {delay: 600})
-sr.reveal(`.profile__social`, {delay: 700})
-sr.reveal(`.profile__info-group`, {interval: 100, delay: 700})
-sr.reveal(`.profile__buttons`, {delay: 800})
-sr.reveal(`.filters__content`, {delay: 900})
-sr.reveal(`.filters`, {delay: 1000})
+sc.reveal(`.profile__border`)
+sc.reveal(`.profile__name`, {delay: 500})
+sc.reveal(`.profile__profession`, {delay: 600})
+sc.reveal(`.profile__social`, {delay: 700})
+sc.reveal(`.profile__info-group`, {interval: 100, delay: 700})
+sc.reveal(`.profile__buttons`, {delay: 800})
+sc.reveal(`.filters__content`, {delay: 900})
+sc.reveal(`.filters`, {delay: 1000})
